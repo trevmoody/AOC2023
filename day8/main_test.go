5c815362398aff8ec62b67066a193389aa15e07c
trevor <trevor@redwood.house>
2023-12-08 15:37:10 +0000

    remove test on real data

--- day8/main_test.go
+++ day8/main_test.go
@@ -25,11 +25,6 @@ func Test_part1(t *testing.T) {
 			args: args{lines: *util.GetFileAsLines("testinput2")},
 			want: 6,
 		},
-		{
-			name: "Real Input",
-			args: args{lines: *util.GetFileAsLines("input")},
-			want: 16343,
-		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
